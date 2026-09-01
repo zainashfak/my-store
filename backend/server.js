f69bfe0a951d2085c2831305338b802ea5d456b3
zainashfak <zainashfak420@gmail.com>
2026-09-01 07:06:56 +0500

    Use Railway's dynamic port

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const Product = require('./models/Product');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
